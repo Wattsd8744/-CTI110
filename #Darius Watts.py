b4318a576b7a2a151f4df2a20f90c9b5a2f7b7ea
Wattsd8744 <dariuswattsdw@gmail.com>
2026-02-07 02:05:17 -0500

    P1LAB1

---
+++ #Darius Watts.py
@@ -0,0 +1,10 @@
+#Darius Watts
+#Plab1
+#input and print!
+#2/7/2026
+#basics of python done in this lab
+
+first_name = input("enter your first name: ") 
+last_name = input("enter your last name: ") 
+
+print("hello, "+  first_name + last_name + "!", "Welcome to CTI")
